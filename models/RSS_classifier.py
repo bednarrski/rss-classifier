5660b8d04128b3022f6488de7fedc99b107152ff
bednarrski <bednarrski@gmail.com>
2017-12-14 16:25:02 +0100

    "Update at 20171214_152502"

--- models/RSS_classifier.py
+++ models/RSS_classifier.py
@@ -1,7 +1,16 @@
 import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+
 #import numpy as np
 
 def classify(articles_info_pd):
+    #models = pickle.load(open( 'rss_models_171214.pickle', "r" ) )
+    #print(models)
+    
     articles_info_pd['class'] = True
     
     articles_info_pd.loc[articles_info_pd['abstract'].str.contains('voice'),['class']] = False
