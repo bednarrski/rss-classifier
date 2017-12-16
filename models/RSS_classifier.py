0b70af2e8ec311b10f6cccc36a797a88f5d6a389
bednarrski <bednarrski@gmail.com>
2017-12-16 06:00:20 +0100

    "Update at 20171216_050020"

--- models/RSS_classifier.py
+++ models/RSS_classifier.py
@@ -19,8 +19,14 @@ def classify2(articles_info_pd):
     models = pickle.load(open( 'rss_models_171215.pickle', "rb" ))
     print(models)
     
-    articles_info_pd['class'] = True
+    articles_info_pd['class'] = 'test'
+    svc = models['svc']
     
-    articles_info_pd.loc[articles_info_pd['abstract'].str.contains('voice'),['class']] = False
+    for i in range(len(articles_info_pd['class'])):
+        temp = articles_info_pd.iloc[i]['title']+' '\
+                +articles_info_pd.iloc[i]['authors']+' '\
+                +articles_info_pd.iloc[i]['abstract']
+        #articles_info_pd.at[i]['class'] = temp
+        articles_info_pd.set_value(i, 'class', temp)
     
     return articles_info_pd
